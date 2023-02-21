1808bd25b39607e2bd7c1882bf11672f80cd31f6
andrey1606 <andrey1606channel@gmail.com>
2023-02-21 22:18:01 +0500

    small fixes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,19 @@
-﻿$(function () {
-    $(".photo-button").on("click", function () {
-        var walkId = $(this).data("id");
-        $.get("/Home/GetPhotos", { walkId: walkId }).done(function (result) {
-            $("#walk-modal .modal-body").html(result);
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var photoButtons = document.querySelectorAll(".photo-button");
+
+    photoButtons.forEach(function (photoButton) {
+        photoButton.addEventListener("click", function () {
+            var walkId = this.getAttribute("data-id");
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/Home/GetPhotos?walkId=" + walkId);
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    document.querySelector(
+                        "#walk-modal .modal-body"
+                    ).innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
         });
     });
 });
